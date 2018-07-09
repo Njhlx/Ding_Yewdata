8b6ff4b38d1a16cb3902a4641be2dd8a17393f5f
hlx <helx_zonglian@126.com>
2018-07-09 15:24:02 +0800

    优化： 1-增加备用通道库； 2-待接收数据包大于10报警

--- ConstSettings.py
+++ ConstSettings.py
@@ -7,8 +7,15 @@ def define():
     # 钉钉机器人地址
     CONST.DINGTALK_ROBOT = "https://oapi.dingtalk.com/robot/send?access_token=" \
                            "801fbba15f1b0559be1c81de38ae4ba2290549a67004cd590cffd9e87c45605e"
+    #通道库地址
+    CONST.TONGDAO_SERVER = ['szbj1dat.sqlserver.rds.aliyuncs.com','njsz1dat.sqlserver.rds.aliyuncs.com']
+    CONST.TONGDAO_PORT = ['3433', '3433']
+    CONST.TONGDAO_DATABASE = ['szbj02data','njsz02data']
+    CONST.TONGDAO_USER = ['szbj02data','njsz02data']
+    CONST.TONGDAO_PASSWORD = ['szbj02_8289986','njsz02_8289986']
+
     #客户名称+客户通道授权码
-    CONST. TONGDAO_CODE = {'郑州金麦':'20170728zzjm','合肥超港':'z9cggs20171124','淮安金鸡':'z9hajj20180418','长沙多喜来':'z9csdxl20171127','巩义花儿':'z9gyhe20180504','宁德艾宁檬':'z9ndanm20171222','南宁一品轩':'z9nnypx20180116'}
+    CONST.TONGDAO_CODE = {'郑州金麦':'20170728zzjm','合肥超港':'z9cggs20171124','淮安金鸡':'z9hajj20180418','长沙多喜来':'z9csdxl20171127','巩义花儿':'z9gyhe20180504','宁德艾宁檬':'z9ndanm20171222','南宁一品轩':'z9nnypx20180116','无锡咔芙美':'z9wxkfm20180518','广州绿叶居':'z9gzlyj20180626'}
     #信道
     CONST.TONGDAO_CHAINS = ['ywdata-801huiz','ywdata-801kuc','ywdata-801caiw','ywdata-801dd','ywdata-801dhk','ywdata-801hsq','ywdata-801mdkd','ywdata-801mdxs','ywdata-801trfv3','ywdata-801wul','ywdata-801zaixzhh']
 
--- DingMsgSend.py
+++ DingMsgSend.py
@@ -6,31 +6,43 @@ CONST = Common.Const
 
 
 def sub_function():
-    # 通道库地址：
-    s_server = "szbj1dat.sqlserver.rds.aliyuncs.com"
-    s_port = "3433"
-    s_database = "szbj02data"
-    s_user = "szbj02data"
-    s_password = "szbj02_8289986"
-
     #将字段客户通道转为两个列表，分别为客户名称、客户CODE
     names = list(ConstSettings.CONST.TONGDAO_CODE.keys())
     codes = list(ConstSettings.CONST.TONGDAO_CODE.values())
     chans = CONST.TONGDAO_CHAINS
+    # print(chans)
 
-    for name in names:
-        # print(name)
-        code = ConstSettings.CONST.TONGDAO_CODE[name]
-        # print(code)
-        row_count = 0
+    # 通道库地址，两个通道库：
+    for i in range(0, 2):
+        # print(i)
+        s_server = CONST.TONGDAO_SERVER[i]
+        s_port = CONST.TONGDAO_PORT[i]
+        s_database = CONST.TONGDAO_DATABASE[i]
+        s_user = CONST.TONGDAO_USER[i]
+        s_password = CONST.TONGDAO_PASSWORD[i]
+        # print(s_server)
 
-        for chan in chans:
-            # print(chan)
-            sSQL = 'select * from [dtv5dtchdan' + code + '_' + chan + ']'
-            # print(sSQL)
-            row_count = row_count + SQLExec.int_exec(s_server, s_port, s_user, s_password, s_database,
-                                         sSQL)
-            # print(row_count)
-        msg = ''
-        msg = msg + name + ": 业务数据仓库待接收数据包：" + str(row_count)
-        DingTalkRobot.send_text(msg)
+        for name in names:
+            # print(name)
+            code = ConstSettings.CONST.TONGDAO_CODE[name]
+            # print(code)
+            row_count = 0
+
+            for chan in chans:
+                # print(chan)
+                sSQL = 'select * from [dtv5dtchdan' + code + '_' + chan + ']'
+                # print(sSQL)
+                row_count = row_count + SQLExec.int_exec(s_server, s_port, s_user, s_password, s_database,
+                                             sSQL)
+                # print(row_count)
+
+            #增加判断，待接收数据包>10才提示
+            if row_count >= 10 :
+                count = row_count
+                msg = ''
+                msg = "tongd" + str(i) + "_" + msg + name +  ": 业务数据仓库待接收数据包：" + str(count)
+                # print(msg)
+                DingTalkRobot.send_text(msg)
+
+
+
